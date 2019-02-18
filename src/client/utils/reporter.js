13e3355661332ef0b50860fd2a91244a6608349c
Maksym Rusynyk <Maksym.Rusynyk@ing.nl>
2019-02-18 08:49:06 +0100

    Fix Copyright

--- src/client/utils/reporter.js
+++ src/client/utils/reporter.js
@@ -1,3 +1,9 @@
+/**
+* Copyright (c) Maksym Rusynyk 2018 - present
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
 /* @flow */
 
 type TestResultType = {
