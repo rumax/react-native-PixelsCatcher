afb286d283a4abeab0502e0469fa97b5b08633dd
Maksym Rusynyk <Maksym.Rusynyk@ing.nl>
2019-03-19 12:34:58 +0100

    Upgrade rn to 0.59 in demo project

--- demo/android/app/src/main/java/com/demo/MainApplication.java
+++ demo/android/app/src/main/java/com/demo/MainApplication.java
@@ -1,13 +1,13 @@
-package com.rumax.pixelscatcher.testapp;
+package com.demo;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rumax.reactnative.saveview.SaveViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.rumax.reactnative.saveview.SaveViewPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,8 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-        new MainReactPackage(),
-        new SaveViewPackage()
+          new MainReactPackage(),
+            new SaveViewPackage()
       );
     }
 
