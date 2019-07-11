95b0c90c12296c2b3960185a692b79ffcfbe80dd
Maksym Rusynyk <Maksym.Rusynyk@ing.nl>
2019-07-12 00:23:31 +0200

    refactor mk_lib script

--- mk_lib.sh
+++ mk_lib.sh
@@ -6,46 +6,40 @@ CMD="./node_modules/.bin/flow-remove-types"
 rm -rf $OUTPUT
 mkdir $OUTPUT
 
-function removeTypes {
-  $CMD "$SRC/$1" --pretty --out-file "$OUTPUT/$1"
+function pathFromFile {
+  echo "${1%/*}"
 }
 
 function copy {
-  cp "$SRC/$1" "$OUTPUT/$1"
+  toPath="$OUTPUT/$(pathFromFile $2)"
+  mkdir -p $toPath
+  cp "$1" "$OUTPUT/$2"
+}
+
+function removeTypes {
+  toPath="$OUTPUT/$(pathFromFile $2)"
+  mkdir -p $toPath
+  $CMD "$1" --pretty --out-file "$OUTPUT/$2"
 }
 
 # Runner
-mkdir "$OUTPUT/runner"
-mkdir "$OUTPUT/runner/utils"
-mkdir "$OUTPUT/runner/utils/device"
-mkdir "$OUTPUT/runner/server"
+copy "src/runner/server/dummy.png" "runner/server/dummy.png"
+files=`find ./src/runner -name '*.js'`
 
-removeTypes "runner/cli.js"
-chmod +x "$OUTPUT/runner/cli.js"
-
-removeTypes "runner/utils/device/AndroidEmulator.js"
-removeTypes "runner/utils/device/IOSSimulator.js"
-removeTypes "runner/utils/delay.js"
-removeTypes "runner/utils/exec.js"
-removeTypes "runner/utils/log.js"
-removeTypes "runner/utils/readConfig.js"
-removeTypes "runner/utils/timeToSec.js"
-
-removeTypes "runner/server/compareImages.js"
-removeTypes "runner/server/server.js"
-copy "runner/server/dummy.png"
+while read -r sourceFile; do
+  outputFile=`echo "$sourceFile" | sed 's/\.\/src\///'`
+  if [[ ( ${outputFile} != *"__tests__"* ) && ( ${outputFile} != *"Interface.js" ) ]]
+  then
+    removeTypes $sourceFile $outputFile
+  fi
+done <<< "$files"
 
 # Client
-mkdir "$OUTPUT/client"
-mkdir "$OUTPUT/client/utils"
+files=`find ./src/client -name '*.js'`
 
-copy "client/Snapshot.js"
-copy "client/SnapshotsContainer.js"
-copy "client/index.js"
-copy "client/snapshotsManager.js"
-
-copy "client/utils/endOfTest.js"
-copy "client/utils/network.js"
-copy "client/utils/compareToReference.js"
-copy "client/utils/log.js"
-copy "client/utils/reporter.js"
+while read -r sourceFile; do
+  outputFile=`echo "$sourceFile" | sed 's/\.\/src\///'`
+  if [[ ${outputFile} != *"__tests__"* ]];then
+    copy $sourceFile $outputFile
+  fi
+done <<< "$files"
