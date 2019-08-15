7ddf6cce5b58eae14c8c4a302db0b7b1190a7e8a
Maksym Rusynyk <Maksym.Rusynyk@ing.nl>
2019-08-15 13:18:32 +0200

    New UI test for long content
    - including gradle small fix

--- demo/index.js
+++ demo/index.js
@@ -5,4 +5,10 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const snapshots = false;
+
+if (snapshots) {
+   require('./indexSnapshot');
+} else {
+  AppRegistry.registerComponent(appName, () => App);
+}
