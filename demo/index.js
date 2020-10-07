99567819d4072dee1ba28e756004ff51d209b8dc
Maksym Rusynyk <m.rusynyk@gmail.com>
2020-10-07 22:39:19 +0200

    Demo project: Upgrade RN to v0.63.3

--- demo/index.js
+++ demo/index.js
@@ -1,14 +1,15 @@
-/** @format */
+/**
+ * @format
+ */
 
 import { AppRegistry } from 'react-native';
-
 import App from './App';
 import { name as appName } from './app.json';
 
-const snapshots = false;
+const snapshots = true;
 
 if (snapshots) {
-   require('./indexSnapshot');
+  require('./indexSnapshot');
 } else {
   AppRegistry.registerComponent(appName, () => App);
 }
