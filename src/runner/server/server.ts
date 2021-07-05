1c72b2ecdf2f652a9ab4681d6c253ed8a50bee92
Maksym Rusynyk <m.rusynyk@gmail.com>
2021-07-05 21:36:13 +0200

    Fix typescript warning

--- src/runner/server/server.ts
+++ src/runner/server/server.ts
@@ -25,6 +25,7 @@ let nextSocketId = 0;
 const mkDir = (directory: string, clean: boolean = false) => {
   if (clean && fs.existsSync(directory)) {
     log.i(TAG, `Cleaning [${directory}]`);
+    // @ts-ignore: https://nodejs.org/api/fs.html#fs_fspromises_rmdir_path_options
     fs.rmdirSync(directory, { recursive: true });
   }
 
